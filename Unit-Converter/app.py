a705729f007378707b941e5df7812670fd933b97
bobthe3rdjrjr <lijope987@gmail.com>
2026-03-10 17:47:15 +1000

    Im strugglin here after only watching a 10m flask tutorial

--- Unit-Converter/app.py
+++ Unit-Converter/app.py
@@ -29,16 +29,17 @@ def temperature():
     conversion_unit = request.args.get("conversion_unit")
     return render_template("temperature.html", base_unit=base_unit, base_amt=base_amt, conversion_unit=conversion_unit)
     
-@app.route("/convert", methods=["get"])
-def convert():
-    pass
-    # Run through validity checker
-    
-        # if valid
-            # Run through backend 
-            # return
-        # else
-            # return false through /valid
+@app.route("/process", methods=["get"])
+def process():
+    if len(request.form) == 1:
+        correct_unit = request.form.get("correct_unit")
+    elif len(request.form) == 3:
+        base_unit = request.form.get("base_unit")
+        base_amt = request.form.get("base_amt")
+        conversion_unit = request.form.get("conversion_unit")
+    else:
+        raise ValueError("PANIC")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
