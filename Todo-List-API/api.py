f5bb38646a336e738819626bfc34b5dbe42376ab
bobthe3rdjrjr <lijope987@gmail.com>
2026-03-28 10:29:18 +1000

    Still doing stuff very confused

--- Todo-List-API/api.py
+++ Todo-List-API/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, make_response, session
+from flask import Flask, render_template, jsonify, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Mapped
 from flask_restful import Resource, Api, reqparse, marshal_with, fields, abort
@@ -19,19 +19,21 @@ api = Api(app)
 
 class UserModel(db.Model):
     id: Mapped[int] = db.Column(db.Integer, primary_key=True)
-    username: Mapped[str] = db.Column(db.String(15), unique=True, nullable=False)
+    name: Mapped[str] = db.Column(db.String(15), unique=True, nullable=False)
     email: Mapped[str] = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
-        return f"User: {self.username}, Email: {self.email}"
+        return f"User: {self.name}, Email: {self.email}"
 
 user_args = reqparse.RequestParser()
-user_args.add_argument('username', type=str, required=True, help="Username is required")
+user_args.add_argument('name', type=str, required=True, help="Name is required")
+user_args.add_argument('password', type=str, required=True, help="Password is required")
 user_args.add_argument('email', type=str, required=True, help="Email is required")
 
 user_fields = {
     'id': fields.Integer,
-    'username': fields.String,
+    'name': fields.String,
+    'password': fields.String,
     'email': fields.String
 }
 
@@ -44,7 +46,7 @@ class Users(Resource):
     @marshal_with(user_fields)
     def post(self):
         args = user_args.parse_args()
-        user = UserModel(username=args["username"], email=args["email"])
+        user = UserModel(name=args["name"], email=args["email"])
         db.session.add(user)
         db.session.commit()
         users = UserModel.query.all()
@@ -64,7 +66,7 @@ class User(Resource):
         user = UserModel.query.filter_by(id=id).first()
         if not user:
             abort(404)
-        user.username = args["username"]
+        user.name = args["name"]
         user.email = args["email"]
         db.session.commit()
         return user
@@ -85,40 +87,46 @@ api.add_resource(Users, "/users")
 def token_required(func):
     @wraps(func)
     def decorated(*args, **kwargs): 
-        token = request/args.get('token')
+        token = request.args.get('token')
         if not token:
             return jsonify({"Alert!": 'Token is Missing.'})
         
         try:
-            payload = jwt.decode(token, jwt_secret)
+            payload = jwt.decode(token, jwt_secret, algorithms=["HS256"])
         except:
             return jsonify({'Alert':"Invalid Token"})
-        return decorated
+        return func(*args, **kwargs)    
+    return decorated
 
 # Define routes:
 @app.route("/")
 def home():
-    if not session.get:
+    if not session.get("logged id"):
         return render_template("login.html")
     return render_template("home.html")
 
 # Login
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.form['username'] and request.form['password']:
-        session['logged id'] = True
-        token = jwt.encode({
-            "user": request.form['username'],
-            "expiration": datetime.now(timezone.utc) + timedelta(seconds=500),
-        },
-        jwt_secret)
-        return jsonify({"token": token.decode("utf-8")})
-    else:
-        return make_response("Unable to verify", 403, {"WWW-Authenticate" : 'Basic realm:"Authentication Failed"'})
+    if request.method == 'POST':
+        name = request.form['name']
+        pw = request.form['password']
+        if name and pw:
+            session['logged id'] = True
+            token = jwt.encode({
+                "user": request.form['name'],
+                "expiration": datetime.now(timezone.utc) + timedelta(seconds=500),
+            },
+            jwt_secret)
+            return jsonify({"token": token})
+        else:
+            return render_template("error.html", message="404, User Not Found")
+    else: 
+        return render_template("login.html")
 
 # Public
-@app.route("/public")
-def public():
+@app.route("/register")
+def register():
     return "For Public"
 
 # Authenticated
